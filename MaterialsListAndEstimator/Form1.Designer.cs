415191f4cf79389a0367bd5caea0a4f0b37d174e
tonyl <tonyl@TONY-PC>
2023-09-26 18:57:48 -0400

    Add project files.

---
+++ MaterialsListAndEstimator/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace MaterialsListAndEstimator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            Column0 = new DataGridViewTextBoxColumn();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(878, 825);
+            button2.Margin = new Padding(4);
+            button2.Name = "button2";
+            button2.Size = new Size(129, 62);
+            button2.TabIndex = 1;
+            button2.Text = "Calculate";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column0, Column1, Column2, Column3 });
+            dataGridView1.Location = new Point(13, 13);
+            dataGridView1.Margin = new Padding(4);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(994, 724);
+            dataGridView1.TabIndex = 20;
+            // 
+            // Column0
+            // 
+            Column0.HeaderText = "Material Name";
+            Column0.Name = "Column0";
+            Column0.Width = 500;
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle1.Format = "C2";
+            dataGridViewCellStyle1.NullValue = "0";
+            Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            Column1.HeaderText = "Unit Cost";
+            Column1.Name = "Column1";
+            Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle2.Format = "N0";
+            dataGridViewCellStyle2.NullValue = "0";
+            Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            Column2.HeaderText = "Quantity";
+            Column2.Name = "Column2";
+            Column2.Resizable = DataGridViewTriState.True;
+            Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            dataGridViewCellStyle3.Format = "C2";
+            dataGridViewCellStyle3.NullValue = "0";
+            Column3.DefaultCellStyle = dataGridViewCellStyle3;
+            Column3.HeaderText = "Subtotal";
+            Column3.Name = "Column3";
+            Column3.ReadOnly = true;
+            Column3.Width = 150;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(445, 825);
+            button1.Margin = new Padding(4);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 62);
+            button1.TabIndex = 0;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(13, 829);
+            button3.Margin = new Padding(4);
+            button3.Name = "button3";
+            button3.Size = new Size(129, 62);
+            button3.TabIndex = 21;
+            button3.Text = "Clear";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(807, 744);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(200, 26);
+            textBox1.TabIndex = 22;
+            textBox1.Text = "0";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(720, 747);
+            label1.Name = "label1";
+            label1.Size = new Size(81, 19);
+            label1.TabIndex = 23;
+            label1.Text = "Grand Total";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1021, 904);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(dataGridView1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button button2;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button3;
+        private TextBox textBox1;
+        private Label label1;
+        private DataGridViewTextBoxColumn Column0;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+    }
+}
---
+++ MaterialsListAndEstimator/Form1.cs
@@ -0,0 +1,161 @@
+using System.Windows.Forms;
+using System.IO;
+using System.Data;
+using System;
+
+namespace MaterialsListAndEstimator
+{
+    public partial class Form1 : Form
+    {
+        private int GrandTotal = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Add event handlers for CellValidating
+            dataGridView1.CellValidating += DataGridView1_CellValidating;
+        }
+
+        private void DataGridView1_CellValidating(object? sender, DataGridViewCellValidatingEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["Column1"].Index || // Unit Cost column
+                e.ColumnIndex == dataGridView1.Columns["Column2"].Index)   // Quantity column
+            {
+                // Convert value in either box to string
+                string value = e.FormattedValue.ToString();
+
+                // If conversion to int fails then it is not an int, throw error
+                if (!int.TryParse(value, out int result))
+                {
+                    e.Cancel = true;
+                    MessageBox.Show("Unit Cost and/or Quantity must be numerical.  Clear the box and try again.", "Error");
+                }
+            }
+        }
+
+        private void MultiplyColumns(object sender, EventArgs e)
+        {
+            // Get the DataGridView
+            DataGridView dataGridView = this.dataGridView1;
+
+            // Get the indexes of the 2nd and 3rd columns
+            int columnIndex1 = dataGridView.Columns["Column1"].Index; // Unit Cost column
+            int columnIndex2 = dataGridView.Columns["Column2"].Index; // Quantity column
+
+            // Loop through all rows
+            for (int i = 0; i < dataGridView.Rows.Count; i++)
+            {
+                // Get the values of the 2nd and 3rd columns
+                int value1 = Convert.ToInt32(dataGridView.Rows[i].Cells[columnIndex1].Value);
+                int value2 = Convert.ToInt32(dataGridView.Rows[i].Cells[columnIndex2].Value);
+
+                // Multiply the values and set the value in the 4th column
+                dataGridView.Rows[i].Cells[3].Value = value1 * value2;
+            }
+        }
+
+        private void AddTotal(object sender, EventArgs e)
+        {
+            // Get the DataGridView
+            DataGridView dataGridView = this.dataGridView1;
+
+            // Get the index of the 4th column
+            int columnIndex3 = dataGridView.Columns["Column3"].Index; //Subtotal column
+
+            // Reset grandTotal
+            GrandTotal = 0;
+
+            // Loop through all rows
+            for (int i = 0; i < dataGridView.Rows.Count; i++)
+            {
+                // Get the values of the 4th columns
+                int value3 = Convert.ToInt32(dataGridView.Rows[i].Cells[columnIndex3].Value);
+
+                // Increment grand total by each subtotal
+                GrandTotal += value3;
+            }
+            // Set value to textBox in bottom left
+            textBox1.Text = GrandTotal.ToString();
+        }
+
+        private void Save(object sender, EventArgs e)
+        {
+            // Create a SaveFileDialog object
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // Set the initial directory to the current directory
+            saveFileDialog.InitialDirectory = Environment.CurrentDirectory;
+
+            // Set the filter to text files.
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+
+            // Show the SaveFileDialog.
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the path and name of the selected file
+                string fileName = saveFileDialog.FileName;
+
+                // Create a StreamWriter object to write to the file
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    string line = "Name, Unit Cost, Quantity, Subtotal \n";
+
+                    //This for loop loops through each row in the table except the last one it is the empty row that always gets created
+                    for (int i = 0; i <= dataGridView1.Rows.Count - 2; i++)
+                    {
+                        //This for loop loops loops loop through each column. Loop.
+                        for (int j = 0; j <= dataGridView1.Columns.Count - 1; j++)
+                        {
+                            line = line + dataGridView1.Rows[i].Cells[j].Value;
+
+                            if (j != dataGridView1.Columns.Count - 1)
+                            {
+                                //A comma and 7 spaced are added to separate each field in the text file
+                                line = line + ",       ";
+                            }
+                        }
+
+                        // check for empty but doesn't work idk, look into it
+                        if (line != "")
+                        {
+                            // Write the text to the file
+                            writer.WriteLine(line);
+                            line = "";
+                        }
+
+                    }
+                }
+            }
+        }
+
+        // Save Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Save(sender, e);
+        }
+
+        // Calculate Button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MultiplyColumns(sender, e);
+            AddTotal(sender, e);
+        }
+
+        // Clear Button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Clear the grid
+            dataGridView1.Rows.Clear();
+
+            // Reset grandTotal
+            GrandTotal = 0;
+
+            // Set value to textBox in bottom left
+            textBox1.Text = GrandTotal.ToString();
+        }
+    }
+}
