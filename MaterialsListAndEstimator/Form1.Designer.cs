5c2070af13bdf28caeca94c798a46bf5780516f8
Anthony Barnes <tonylbarnes@gmail.com>
2023-10-02 17:38:03 -0400

    Adding Samuel Jordan Clark's anchor code changes, adjusting columns to autosize

--- MaterialsListAndEstimator/Form1.Designer.cs
+++ MaterialsListAndEstimator/Form1.Designer.cs
@@ -33,24 +33,25 @@
             DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
             button2 = new Button();
             dataGridView1 = new DataGridView();
-            Column0 = new DataGridViewTextBoxColumn();
-            Column1 = new DataGridViewTextBoxColumn();
-            Column2 = new DataGridViewTextBoxColumn();
-            Column3 = new DataGridViewTextBoxColumn();
             button1 = new Button();
             button3 = new Button();
             textBox1 = new TextBox();
             label1 = new Label();
+            Column0 = new DataGridViewTextBoxColumn();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // button2
             // 
+            button2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             button2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.Location = new Point(878, 825);
+            button2.Location = new Point(912, 525);
             button2.Margin = new Padding(4);
             button2.Name = "button2";
-            button2.Size = new Size(129, 62);
+            button2.Size = new Size(95, 46);
             button2.TabIndex = 1;
             button2.Text = "Calculate";
             button2.UseVisualStyleBackColor = true;
@@ -59,57 +60,24 @@
             // dataGridView1
             // 
             dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column0, Column1, Column2, Column3 });
             dataGridView1.Location = new Point(13, 13);
             dataGridView1.Margin = new Padding(4);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(994, 724);
+            dataGridView1.Size = new Size(994, 404);
             dataGridView1.TabIndex = 20;
-            // 
-            // Column0
-            // 
-            Column0.HeaderText = "Material Name";
-            Column0.Name = "Column0";
-            Column0.Width = 500;
-            // 
-            // Column1
-            // 
-            dataGridViewCellStyle1.Format = "C2";
-            dataGridViewCellStyle1.NullValue = "0";
-            Column1.DefaultCellStyle = dataGridViewCellStyle1;
-            Column1.HeaderText = "Unit Cost";
-            Column1.Name = "Column1";
-            Column1.Width = 150;
-            // 
-            // Column2
-            // 
-            dataGridViewCellStyle2.Format = "N0";
-            dataGridViewCellStyle2.NullValue = "0";
-            Column2.DefaultCellStyle = dataGridViewCellStyle2;
-            Column2.HeaderText = "Quantity";
-            Column2.Name = "Column2";
-            Column2.Resizable = DataGridViewTriState.True;
-            Column2.Width = 150;
-            // 
-            // Column3
-            // 
-            dataGridViewCellStyle3.Format = "C2";
-            dataGridViewCellStyle3.NullValue = "0";
-            Column3.DefaultCellStyle = dataGridViewCellStyle3;
-            Column3.HeaderText = "Subtotal";
-            Column3.Name = "Column3";
-            Column3.ReadOnly = true;
-            Column3.Width = 150;
             // 
             // button1
             // 
+            button1.Anchor = AnchorStyles.Bottom;
             button1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(445, 825);
+            button1.Location = new Point(460, 525);
             button1.Margin = new Padding(4);
             button1.Name = "button1";
-            button1.Size = new Size(129, 62);
+            button1.Size = new Size(97, 46);
             button1.TabIndex = 0;
             button1.Text = "Save";
             button1.UseVisualStyleBackColor = true;
@@ -117,11 +85,12 @@
             // 
             // button3
             // 
+            button3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             button3.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button3.Location = new Point(13, 829);
+            button3.Location = new Point(13, 525);
             button3.Margin = new Padding(4);
             button3.Name = "button3";
-            button3.Size = new Size(129, 62);
+            button3.Size = new Size(100, 46);
             button3.TabIndex = 21;
             button3.Text = "Clear";
             button3.UseVisualStyleBackColor = true;
@@ -129,27 +98,68 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(807, 744);
+            textBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            textBox1.Location = new Point(807, 424);
             textBox1.Name = "textBox1";
             textBox1.ReadOnly = true;
             textBox1.Size = new Size(200, 26);
             textBox1.TabIndex = 22;
-            textBox1.Text = "0";
+            textBox1.Text = "$0";
             // 
             // label1
             // 
+            label1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             label1.AutoSize = true;
-            label1.Location = new Point(720, 747);
+            label1.Location = new Point(720, 427);
             label1.Name = "label1";
             label1.Size = new Size(81, 19);
             label1.TabIndex = 23;
             label1.Text = "Grand Total";
             // 
+            // Column0
+            // 
+            Column0.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column0.FillWeight = 500F;
+            Column0.HeaderText = "Material Name";
+            Column0.Name = "Column0";
+            // 
+            // Column1
+            // 
+            Column1.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle1.Format = "C2";
+            dataGridViewCellStyle1.NullValue = "0";
+            Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            Column1.FillWeight = 50F;
+            Column1.HeaderText = "Unit Cost";
+            Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            Column2.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle2.Format = "N0";
+            dataGridViewCellStyle2.NullValue = "0";
+            Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            Column2.FillWeight = 50F;
+            Column2.HeaderText = "Quantity";
+            Column2.Name = "Column2";
+            Column2.Resizable = DataGridViewTriState.True;
+            // 
+            // Column3
+            // 
+            Column3.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle3.Format = "C2";
+            dataGridViewCellStyle3.NullValue = "0";
+            Column3.DefaultCellStyle = dataGridViewCellStyle3;
+            Column3.FillWeight = 150F;
+            Column3.HeaderText = "Subtotal";
+            Column3.Name = "Column3";
+            Column3.ReadOnly = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1021, 904);
+            ClientSize = new Size(1021, 584);
             Controls.Add(label1);
             Controls.Add(textBox1);
             Controls.Add(button3);
